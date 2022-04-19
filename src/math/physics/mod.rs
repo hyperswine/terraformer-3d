d36d77ada975d3fef59b6b1cd6e6ad0315231204
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-04-19 15:35:29 +1000

    Add more stuff on physics

--- src/math/physics/mod.rs
+++ src/math/physics/mod.rs
@@ -1,4 +1,6 @@
-// wrappers around rust physics
+// ---------
+// COLLISION DETECTION
+// ---------
 
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 pub struct BoundingBox3D {
@@ -7,8 +9,7 @@ pub struct BoundingBox3D {
     max_axes: (f32, f32, f32),
 }
 
-#[inline]
-fn intersect(box_pair: &[BoundingBox3D; 2]) {}
+use std::num;
 
 use itertools::Itertools;
 
@@ -32,12 +33,39 @@ pub fn check_collision(objects: &[BoundingBox3D]) {
     }
 }
 
-// A 3d collidable object with a bounding box
-// pub trait CollisionMechanics3D {}
+// ---------
+// TRAJECTORY CALCS
+// ---------
 
-// * example implementation: CharacterObject
-// struct CharacterObject {
-//     bounding_box: BoundingBox3D
-// }
+// earth gravitational accel
+const GRAVITATIONAL_ACCEL: f32 = 9.81;
+const G_VEC: Vec3D = Vec3D::new(0.0, -GRAVITATIONAL_ACCEL, 0.0);
 
-// impl CollisionMechanics3D for CharacterObject {}
+use nalgebra::Vector3;
+
+type Vec3D = Vector3<f32>;
+
+// given prev velocity and acceleration, calc next timestep
+// without external forces
+#[inline]
+fn verlet_step(dt: f32, prev_pos: Vec3D, prev_v: Vec3D, prev_a: Vec3D, curr_a: Vec3D) -> Vec3D {
+    // next velocity
+    let next_v = prev_v + (prev_a + curr_a) / 2.0 * dt;
+    // next position
+    let next_pos = prev_pos + prev_v * dt + 0.5 * prev_a * (dt.powi(2));
+
+    next_pos
+}
+
+// a typical projectile on earth with initial velocity, pos and no accel except G_VEC
+fn bullet_step(dt: f32, prev_pos: Vec3D, prev_v: Vec3D, prev_a: Vec3D, mass: f32) -> Vec3D {
+    // a = F/m = G/mass
+    let curr_a = G_VEC / mass;
+
+    // next velocity
+    let next_v = prev_v + (prev_a + curr_a) / 2.0 * dt;
+    // next position
+    let next_pos = prev_pos + prev_v * dt + 0.5 * prev_a * (dt.powi(2));
+
+    next_pos
+}
